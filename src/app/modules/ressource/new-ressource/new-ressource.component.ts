05b7e3935aa43ca7ec13274259ddc291c70deed5
Notephil <ngongang_philippe@yahoo.fr>
2023-12-13 01:23:59 +0100

    correction bug sur ressources
    autocompletion ko on l'intégre en tant que formcontrol du formbuilder
    nettoyage du code

--- src/app/modules/ressource/new-ressource/new-ressource.component.ts
+++ src/app/modules/ressource/new-ressource/new-ressource.component.ts
@@ -2,13 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { TypeUnite } from 'src/app/modele/type-unite';
 
 import { IRessource } from 'src/app/modele/ressource';
 import { RessourcesService } from 'src/app/services/ressources/ressources.service';
 import {v4 as uuidv4} from 'uuid';
-import { MatTableDataSource } from '@angular/material/table';
-
 import { EMPTY, Observable } from 'rxjs';
 import { IFamille } from 'src/app/modele/famille';
 import { FamillesService } from 'src/app/services/familles/familles.service';
@@ -24,16 +21,7 @@ export class NewRessourceComponent implements OnInit {
   forme: FormGroup;
   btnLibelle: string="Ajouter";
   submitted: boolean=false;
-  ressources$:Observable<IFamille>=EMPTY;
-  myControl = new FormControl<string | IFamille>('');
   filteredOptions: IFamille[] | undefined;
-  dataSource = new MatTableDataSource<IFamille>();
-  familleDeRessource: IFamille = {
-    id: '',
-    libelle: '',
-    description: '',
-    etat:false
-  };
   titre:string=''
   constructor(private formBuilder:FormBuilder,private familleService:FamillesService,private dataEnteteMenuService:DonneesEchangeService,private ressourceService:RessourcesService,private serviceRessource:RessourcesService,private serviceFamille:FamillesService,private router:Router, private infosPath:ActivatedRoute, private datePipe: DatePipe) {
     this.forme = this.formBuilder.group({
@@ -42,16 +30,13 @@ export class NewRessourceComponent implements OnInit {
       quantite: [''],
       unite: [''],
       prix: [''],
-      famille: [''],
+      famille: new FormControl<string | IFamille>(''),
       caracteristique:['']
     })
   };
 
   ngOnInit(): void {
-    this.getAllFamilles().subscribe(valeurs => {
-      this.dataSource.data = valeurs;
-    });
-    this.myControl.valueChanges.subscribe(
+    this.forme.controls["famille"].valueChanges.subscribe(
       value => {
         const libelle = typeof value === 'string' ? value : value?.libelle;
         if(libelle != undefined && libelle?.length >0){
@@ -91,15 +76,6 @@ export class NewRessourceComponent implements OnInit {
     return this.forme.controls;
   }
 
-  getIdFamille(id_famille : string){
-
-    this.serviceFamille.getFamilleById(id_famille).subscribe(
-      famille =>{
-        this.familleDeRessource = famille
-      }
-    )
-  }
-
   onSubmit(ressourceInput:any){
     this.submitted=true;
     if(this.forme.invalid) return;
@@ -117,30 +93,17 @@ export class NewRessourceComponent implements OnInit {
     if(this.ressource != undefined){
       ressourceTemp.id = this.ressource.id
     }
-    ressourceTemp.famille = this.familleDeRessource
-    console.log('voici la famille de cette ressource : ', ressourceTemp.famille)
+
     this.ressourceService.ajouterRessource(ressourceTemp).subscribe(
       object => {
         this.router.navigate(['list-ressources']);
-      },
-      error =>{
-        console.log(error)
       }
     )
   }
-  private getAllFamilles(){
-    return this.serviceFamille.getAllFamilles();
-  }
+
   displayFn(famille: IFamille): string {
     return famille && famille.libelle ? famille.libelle : '';
   }
-  public rechercherListingFamille(option: IFamille){
-    this.serviceFamille.getFamillesByLibelle(option.libelle.toLowerCase()).subscribe(
-        valeurs => {this.dataSource.data = valeurs;}
-    )
-  }
-}
-function getIdfamille(idfamille: any, string: any) {
-  throw new Error('Function not implemented.');
+  
 }
 
