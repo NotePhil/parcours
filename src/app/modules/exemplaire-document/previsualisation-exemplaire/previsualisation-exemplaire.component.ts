0f23c41cd3ecd619d06c82386fedad633edb46f5
Adeline13 <ngomakandaadeline@gmail.com>
2024-02-21 00:02:40 +0100

    previsualisation, ok

--- src/app/modules/exemplaire-document/previsualisation-exemplaire/previsualisation-exemplaire.component.ts
+++ src/app/modules/exemplaire-document/previsualisation-exemplaire/previsualisation-exemplaire.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IExemplaireDocument } from 'src/app/modele/exemplaire-document';
 import { IMouvement } from 'src/app/modele/mouvement';
@@ -36,6 +37,18 @@ export class PrevisualisationExemplaireComponent  implements OnInit {
   };
   titre:string='';
   mouvements : IMouvement[] = []
+  typeNeutre : string = TypeMouvement.Neutre
+  typeAjout : string = TypeMouvement.Ajout
+  typeReduire : string = TypeMouvement.Reduire
+  montantTotal: number = 0;
+  displayedRessourcesColumns: string[] = [
+    'libelle',
+    'quantite',
+    'unite',
+    'description'
+  ]; // structure du tableau presentant les Ressources
+  dataSourceMouvements = new MatTableDataSource<IMouvement>();
+
   constructor(
     private router:Router, 
     private infosPath:ActivatedRoute,
@@ -53,6 +66,10 @@ export class PrevisualisationExemplaireComponent  implements OnInit {
           if (this.exemplaire.mouvements != undefined) {
             this.mouvements = this.exemplaire.mouvements
           }
+          if (this.exemplaire.mouvements != undefined) {
+            this.dataSourceMouvements.data = this.exemplaire.mouvements
+          }
+          this.formerEnteteTableauMissions();
         });
     }
     this.titre=this.dataEnteteMenuService.dataEnteteMenu
@@ -70,7 +87,7 @@ export class PrevisualisationExemplaireComponent  implements OnInit {
     ) {
       const element = this.exemplaire.objetEnregistre[index];
       if (element.key.id == idAttribut) {
-        if (typeAttribut == IType.Date && (element.value != "PARCOURS_NOT_FOUND_404" || element.value != undefined)) {  // si le type de l'attribut est Date et que la valeur n'est pas vide
+        if (typeAttribut == IType.Date && element.value != undefined) {  // si le type de l'attribut est Date et que la valeur n'est pas vide
           let dateAtt = new Date();
           dateAtt = new Date(element.value); // creatoion d'une nouvelle date avec la valeur de valAttribut
 
@@ -81,9 +98,47 @@ export class PrevisualisationExemplaireComponent  implements OnInit {
         return element.value;
       }
     }
-    return 'PARCOURS_NOT_FOUND_404';
+    return '';
   }
   
-  
+  /**
+   * Methodr qui permet de faire la somme des montants du tableau de mouvements
+   * pour afficher le resultat dans la case montant total
+   */
+  sommeMontants(): number {
+    this.montantTotal = 0;
+    this.exemplaire.mouvements?.forEach((mouvement) => {
+      if (
+        mouvement.ressource != undefined &&
+        mouvement.quantite != null &&
+        mouvement.prix != null
+      ) {
+        this.montantTotal += mouvement.prix * mouvement.quantite;
+      }
+    });
+    return this.montantTotal;
+  }
+  /**
+   * Methode qui permet de rajouter les colones de prix et montants si affichePrix a la valeur true
+   */
+  formerEnteteTableauMissions(){
+    if (this.exemplaire.contientDistributeurs == true) {
+      let distributeur : string = "distributeur"
+      this.displayedRessourcesColumns.push(distributeur)
+    }
+    if ((this.exemplaire.affichagePrix == true)) {
+      let prix : string = "prix"
+      let montant : string = "montant total"
+      if (this.exemplaire.typeMouvement == TypeMouvement.Reduire) {
+        prix = "prixDeSortie"
+      } else if (this.exemplaire.typeMouvement == TypeMouvement.Ajout){
+        prix = "prixEntrée"
+      }else{
+        prix = "prix"
+      }
+      this.displayedRessourcesColumns.push(prix)
+      this.displayedRessourcesColumns.push(montant)
+    }
+  }
 }
 
