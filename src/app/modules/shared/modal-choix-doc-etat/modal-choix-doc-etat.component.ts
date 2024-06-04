5be40e9a419457646bd4a1fc4cb0e1663f4ab684
Adeline13 <ngomakandaadeline@gmail.com>
2024-06-04 18:13:35 +0200

    correction dûes au merge.

--- src/app/modules/shared/modal-choix-doc-etat/modal-choix-doc-etat.component.ts
+++ src/app/modules/shared/modal-choix-doc-etat/modal-choix-doc-etat.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { IDocument } from 'src/app/modele/document';
 import { IDocEtats } from 'src/app/modele/doc-etats';
+import { DocumentService } from 'src/app/services/documents/document.service';
 
 interface DialogData {
   EtatsChoisi: IDocEtats[];
@@ -18,14 +19,15 @@ interface DialogData {
 export class ModalChoixDocEtatComponent {
   selectedEtatsMap: IDocEtats | undefined;
   formeEtat: FormGroup;
-  selectedEtat: IDocEtats | undefined;
+  selectedEtat: string = '';
   @Output() saveChanges: EventEmitter<IDocEtats> = new EventEmitter<IDocEtats>();
   @Input() previouslySelectedEtat: IDocEtats | undefined; // Input to receive the previously selected etat
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private fb: FormBuilder,
-    public dialogRef: MatDialogRef<ModalChoixDocEtatComponent>
+    public dialogRef: MatDialogRef<ModalChoixDocEtatComponent>,
+    private documentService: DocumentService // Inject DocumentService
   ) {
     this.formeEtat = this.fb.group({});
   }
